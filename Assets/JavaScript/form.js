bf231a7068b34d23b8c8aa0ac5dcae39bd7379a2
dayvidtp <dayvid.pinto0@gmail.com>
2024-07-03 15:18:03 -0300

    finished-emailjs-form

--- Assets/JavaScript/form.js
+++ Assets/JavaScript/form.js
@@ -1,24 +1,3 @@
-/*document.getElementById("form").addEventListener("submit", function (event) {
-    event.preventDefault(); //Impede o envio padrão do formulario
-
-    //Configuração do email
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "dayvid.pinto0@gmail.com",
-        Password : "E3B98DA500C12120E44EF2FCEA51ED89AE3C",
-        to: 'dayvid.pinto0gmail.com',
-        From: document.getElementById("user-email").value,
-        Subject: "Nova mensagem no seu portfolio",
-        Body: "Nome: " + document.getElementById("user-name").value + " <br>Mensagem: " + document.getElementById("user-message").value
-    }).then(
-        message => {
-        alert("Feito");
-    }
-    )
-})*/
-
-
-
         (function () {
             // https://dashboard.emailjs.com/admin/account
             emailjs.init({
@@ -34,9 +13,9 @@
                 // these IDs from the previous steps
                 emailjs.sendForm('service_m3my7df', 'template_sq6t7tu', this)
                     .then(() => {
-                        alert('SUCCESS!');
+                        alert('Mensagem Enviada!');
                     }, (error) => {
-                        alert('FAILED...', error);
+                        alert('Algo deu errado...', error);
                     });
             });
         }
